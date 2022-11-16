8a471a938097817cc778dbe8e4b809d7f91bf436
Akhilesh Bellathur <abellath@usc.edu>
2022-11-15 21:21:15 -0800

    Errors :(

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,3 +10,35 @@ function App() {
 }
 
 export default App
+
+// import logo from "../logo.svg";
+// import "../styles/App.scss";
+
+// function App() {
+// return (
+// 	<div className="App">
+// 	<header className="App-header">
+// 		<img src={logo} className="App-logo"
+// 			alt="logo" />
+		
+// <p>A simple React app.....</p>
+
+// 		<a
+// 		className="App-link"
+// 		href="https://reactjs.org"
+// 		target="_blank"
+// 		rel="noopener noreferrer"
+// 		>
+// 		Learn React
+// 		</a>
+// 		<form action="../../post" method="post"
+// 			className="form">
+// 		<button type="submit">Connected?</button>
+// 		</form>
+// 	</header>
+// 	</div>
+// );
+// }
+
+// export default App;
+
--- client/src/components/DrawingPanel.js
+++ client/src/components/DrawingPanel.js
@@ -2,6 +2,8 @@ import React, { useRef } from "react"
 import "../styles/drawingPanel.scss"
 import Row from "./Row"
 
+const axios = require('axios').default;
+
 export default function DrawingPanel(props) {
   const { width, height, selectedColor } = props
 
@@ -25,15 +27,43 @@ export default function DrawingPanel(props) {
     }
 
     // Send data via server to RPI
-    console.log(data)
+    console.log("Sending request", data);
+    axios.post("http://localhost:8080/draw", data).then((response) => {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
+
+  const onSubmit = React.useCallback(async () => {
+    let data = []
+    for (const row of panelRef.current.childNodes.values()) {
+      let newRow = []
+      for (const pixel of row.childNodes) {
+        let val = (pixel.attributes.style.nodeValue === "background-color: rgb(255, 255, 255);") ? 0 : 1;
+        newRow.push(val)
+      }
+      data.push(newRow)
+    }
+
+    // Send data via server to RPI
+    console.log("Sending request", data);
+    axios.post("http://localhost:8080/post").then((response) => {
+      console.log("HERHE")
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  });
   
   return (
     <div id="drawingPanel">
       <div id="pixels" ref={panelRef}>
         {rows}
       </div>
-      <button onClick={() => sendImage()} className="button">
+      <button onClick={onSubmit} className="button">
         Send Image
       </button>
     </div>
